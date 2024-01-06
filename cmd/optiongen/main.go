3fc3f5d70f9725cdf7b01f9d7cd0fe0dea66dd02
hauntedness <59566708+hauntedness@users.noreply.github.com>
2024-01-06 20:35:37 +0800

    exe name

--- cmd/optiongen/main.go
+++ cmd/optiongen/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 
 	"github.com/hauntedness/optiongen"
 )
@@ -12,6 +13,9 @@ func main() {
 	packagePath := flag.String("p", ".", "the package name to gen options, default to current package")
 	varPostfix := flag.String("t", "", `specify the variables postfix, default ""`)
 	flag.Parse()
+	if *typeName == "" {
+		log.Fatal("type name is mandatory: optiongen -o someoption")
+	}
 	generated := optiongen.ExecuteString(*typeName, *packagePath, optiongen.WithPostfix(*varPostfix))
 	fmt.Println(generated)
 }
